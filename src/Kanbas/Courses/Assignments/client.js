0ed117a096eb1c53e31e90ee694f1abe4fa3e3e9
wangjiayususan1997 <wangjiayususan@gmail.com>
2023-12-02 18:48:51 -0800

    try change API

--- src/Kanbas/Courses/Assignments/client.js
+++ src/Kanbas/Courses/Assignments/client.js
@@ -1,9 +1,12 @@
 import axios from "axios";
-// const COURSES_URL = "http://localhost:4000/api/courses";
-// const ASSIGNMENTS_URL = "http://localhost:4000/api/assignments";
-const API_BASE = process.env.REACT_APP_API_BASE;
-const COURSES_URL = `${API_BASE}/courses`;
-const ASSIGNMENTS_URL = `${API_BASE}/assignments`;
+//API = "https://kanbas-node-server-app-sl11.onrender.com/a5/todos"
+const COURSES_URL =
+  "https://kanbas-node-server-app-sl11.onrender.com/api/courses";
+const ASSIGNMENTS_URL =
+  "https://kanbas-node-server-app-sl11.onrender.com/api/assignments";
+// const API_BASE = process.env.REACT_APP_API_BASE;
+// const COURSES_URL = `${API_BASE}/courses`;
+// const ASSIGNMENTS_URL = `${API_BASE}/assignments`;
 
 export const findAssignmentsForCourse = async (courseId) => {
   const response = await axios.get(`${COURSES_URL}/${courseId}/assignments`);
