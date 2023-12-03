cea92dee9877dd98e2ea6378622221ab6429bf7e
wangjiayususan1997 <wangjiayususan@gmail.com>
2023-12-02 16:04:21 -0800

    add

--- src/Labs/a5/index.js
+++ src/Labs/a5/index.js
@@ -1,12 +1,15 @@
 import EncodingParametersInURLs from "./EncodingParametersInURLs";
 import WorkingWithObjects from "./WorkingWithObjects";
 import WorkingWithArrays from "./WorkingWithArrays";
+const API_BASE = process.env.REACT_APP_API_BASE;
+const welcome_URL = `${API_BASE}/a5/welcome`;
+
 function Assignment5() {
   return (
     <div>
       <h1>Assignment 5</h1>
       <div className="list-group">
-        <a href="http://localhost:4000/a5/welcome" className="list-group-item">
+        <a href={welcome_URL} className="list-group-item">
           Welcome
         </a>
       </div>
