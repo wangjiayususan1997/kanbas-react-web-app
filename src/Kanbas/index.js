577abd2343d2c33ae644e6495b3208a77df7e3cc
wangjiayususan1997 <wangjiayususan@gmail.com>
2023-12-02 18:58:28 -0800

    add

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -11,7 +11,7 @@ import axios from "axios";
 
 function Kanbas() {
   const [courses, setCourses] = useState([]);
-  const URL = "http://localhost:4000/api/courses";
+  const URL = "https://kanbas-node-server-app-sl11.onrender.com/api/courses";
   const findAllCourses = async () => {
     const response = await axios.get(URL);
     setCourses(response.data);
